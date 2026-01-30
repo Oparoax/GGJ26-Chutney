a194a123ace2b0e5bd197c74b50b0b7f785b8ca4
Byron Crowhurst <bypossum@gmail.com>
2026-01-30 21:02:20 +0000

    HouseDweller prefab created and it now searches for a point near the player.

--- Assets/Scripts/AI/HouseDweller.cs
+++ Assets/Scripts/AI/HouseDweller.cs
@@ -5,21 +5,28 @@ public class HouseDweller : MonoBehaviour
 {
     private Vector3 m_target;
     private NavMeshAgent m_agent;
+    private GameObject m_player;
+    private const float TESTOFFSET = 3f;
+
 
     void Start()
     {
         m_agent = GetComponent<NavMeshAgent>();
-        Vector3 targ = new Vector3().RandomVector(-10, 10);
-        targ.y = -1;
-        m_target = targ;
-        print(targ);
-        TellMeWereToGo(targ);
+        var rac = FindFirstObjectByType<RacoonMove>();
+        m_player = rac ? rac.gameObject : gameObject;
+        m_target = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x == m_target.x && transform.position.z == m_target.z)
+        {
+            m_target = GetPlayerPos() + Vector3.one.RandomVector(-TESTOFFSET, TESTOFFSET);
+            m_target.y = -1;
+            print(m_target);
+            TellMeWereToGo(m_target);
+        }
     }
     
     /// <summary>
@@ -30,11 +37,17 @@ public class HouseDweller : MonoBehaviour
     {
         if (m_agent.SetDestination(_point)){
             print("Moving");
-        }       
+        }
+    }
+
+    private Vector3 GetPlayerPos()
+    {
+        return m_player.transform.position;
     }
 
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.green;
         Gizmos.DrawCube(m_target, Vector3.one * 0.5f);
     }
 }
