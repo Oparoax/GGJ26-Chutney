0a293dc7c192daf1cc3aec75eab97c6d4ff7befe
JDawes <j.dawes@falmouth.ac.uk>
2026-01-31 13:23:08 +0000

    Added interaction code for collector

--- Assets/Scripts/Utility/TrashCollector.cs
+++ Assets/Scripts/Utility/TrashCollector.cs
@@ -5,31 +5,38 @@ public class TrashCollector : MonoBehaviour
 {
     [SerializeField] private GameObject trashBag;
     
-    BoxCollider grabBox;
+    private BoxCollider _grabBox;
+    private IGripTrash gripTrash;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        grabBox = GetComponent<BoxCollider>();
+        Debug.Log("Awake");
+        if (_grabBox == null)
+        {
+            _grabBox = GetComponent<BoxCollider>();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // Checkl for trash
         if (other.CompareTag("Trash"))
         {
-            IGripTrash gripTrash;
+            Debug.Log("Trash Found");
             
+            // Try to grab the IGripTrash Component on the trash object
             try
             {
                 gripTrash = other.gameObject.GetComponent<IGripTrash>();
+                gripTrash.Action(trashBag);
+                Debug.Log("Action called");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
             }
-            
-            gripTrash.Action(trashBag);
         }
     }
 }
