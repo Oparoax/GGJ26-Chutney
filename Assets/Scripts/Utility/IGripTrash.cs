9edc9dabfb063b42432cdbc3abd910df4d72f510
JDawes <j.dawes@falmouth.ac.uk>
2026-01-31 13:24:05 +0000

    Fixed lerping for grab

--- Assets/Scripts/Utility/IGripTrash.cs
+++ Assets/Scripts/Utility/IGripTrash.cs
@@ -2,40 +2,40 @@ using UnityEngine;
 
 public class IGripTrash : IGripper
 {
-    [SerializeField] [Range(0.1f,10.0f)] float grabSpeed = 0.2f;
-    [SerializeField] [Range(0.1f,10.0f)] float grabScaleSpeed = 0.2f;
-    [SerializeField] [Range(0.01f, 1.0f)] private float killDistanceThreshold = 0.1f;
+    [SerializeField] [Range(0.01f, 1.0f)] private float grabSpeed = 0.2f;
+    [SerializeField] [Range(0.01f, 1.0f)] private float grabScaleSpeed = 0.2f;
+    //[SerializeField] [Range(0.01f, 1.0f)] private float killDistanceThreshold = 0.1f;
+    
+    [SerializeField] [Range(0.1f, 2.0f)] private float maxTime = 0.7f;
 
     [SerializeField] private Vector3 finalScale;
-    
-    [SerializeField] SphereCollider modelCollider;
+
+    [SerializeField] private SphereCollider modelCollider;
+    [SerializeField] private Rigidbody _rb;
 
     [SerializeField] private float mass = 1f;
     
-    private bool isGripped;
-    private GameObject gripper;
-    
-    protected override void StartChild()
-    {
-        if (modelCollider == null)
-        {
-            modelCollider =  GetComponent<SphereCollider>();
-        }
-    }
+    private bool _isGripped;
+    private GameObject _gripper;
+
+    private float timeAtGrab;
 
     private void Update()
     {
-        if (isGripped)
+        if (_isGripped)
         {
-            transform.position = Vector3.Slerp(transform.position, transform.TransformDirection(gripper.transform.position), grabSpeed);
-            transform.localScale = Vector3.Slerp(transform.localScale, finalScale, grabScaleSpeed);
+            // Lerp scale and position towards the player.
+            transform.position = Vector3.Lerp(transform.position, transform.TransformDirection(_gripper.transform.position), grabSpeed);
+            transform.localScale = Vector3.Lerp(transform.localScale, finalScale, grabScaleSpeed);
 
-            if (Vector3.Distance(transform.position, gripper.transform.position) < killDistanceThreshold)
+            // Trash will despawn after a set timer.
+            if (Time.time - timeAtGrab > maxTime)
             {
                 // Once close enough to the player send a ui signal and kill the object.
                 
                 // TODO: FIRE UI SIGNAL
                 
+                _isGripped = false;
                 Destroy(gameObject);
             }
         }
@@ -43,7 +43,19 @@ public class IGripTrash : IGripper
 
     public override void Action(GameObject player)
     {
-        isGripped = true;
-        gripper = player;
+        // Activate loop
+        _isGripped = true;
+        
+        // Get Player for location data
+        _gripper = player;
+
+        // Disable collisions & physics
+        interactCollider.enabled = false;
+        modelCollider.enabled = false;
+        
+        _rb.isKinematic = true;
+
+        // Grab time at grab for timer
+        timeAtGrab = Time.time;
     }
 }
