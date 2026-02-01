102d77392d7dafe46b92b7bc3193dd906dfe30bd
JDawes <j.dawes@falmouth.ac.uk>
2026-02-01 12:52:39 +0000

    Created swiper script

---
+++ Assets/Scripts/Utility/Swiper.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Swiper : MonoBehaviour
+{
+    Rigidbody _rb;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (_rb == null)
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Trash"))
+        {
+            var otherRB =  other.GetComponent<Rigidbody>();
+            otherRB.AddForce(,ForceMode.Impulse);
+        }
+    }
+}
+
